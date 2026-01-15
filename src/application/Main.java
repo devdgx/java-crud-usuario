19437027b1c0dbd50bd74b74e2a6fc30b1b75031
Diego <devdgxx@gmail.com>
2026-01-14 23:11:20 -0400

    feat: inicializar aplicação no Main conectando controller, service e repository

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,16 @@
 package application;
 
+import controller.UsuarioController;
+import repository.UsuarioRepository;
+import service.UsuarioService;
+
 public class Main {
+    public static void main(String[] args) {
+        UsuarioRepository usuarioRepository = new UsuarioRepository();
+        UsuarioService usuarioService = new UsuarioService(usuarioRepository);
+        UsuarioController usuarioController = new UsuarioController(usuarioService);
+
+
+        usuarioController.inicarPainelDeMenu();
+    }
 }
