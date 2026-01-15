1917b3fbb57af40f7ff53a5038fa0b717f145b1a
Diego <devdgxx@gmail.com>
2026-01-14 22:53:24 -0400

    feat: implementar UsuarioService com método para adicionar usuário

--- src/service/UsuarioService.java
+++ src/service/UsuarioService.java
@@ -1,4 +1,32 @@
 package service;
 
+import controller.UsuarioController;
+import repository.UsuarioRepository;
+
+import java.util.Scanner;
+
 public class UsuarioService {
+
+    private UsuarioRepository usuarioRepository;
+    Scanner scanner = new Scanner(System.in);
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void adicionarUsuario(){
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Idade: ");
+        int idade = scanner.nextInt();
+
+        usuarioRepository.adicionarNovoUsuario(nome,email,idade);
+
+    }
+
+
 }
