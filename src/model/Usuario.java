07f0df1a112408647096456ee244ff2f61cef55a
Diego <devdgxx@gmail.com>
2026-01-15 19:54:19 -0400

    feat: adicionar classe Usuario com atributos e construtores

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,10 +1,34 @@
 package model;
 
+import java.util.Random;
+
 public class Usuario {
 
     private int id;
     private String nome;
     private int idade;
     private String email;
+    private static Random random = new Random();
 
+    public Usuario(String email) {
+        this.email = email;
+    }
+
+    public Usuario (String nome, int idade, String email){
+        this.id = random.nextInt(10)+1;
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "Nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", email='" + email + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
