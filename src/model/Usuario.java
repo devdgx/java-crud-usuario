0023caeadec3e811d5ec96a66f61def53f720878
Diego <devdgxx@gmail.com>
2026-01-16 00:01:51 -0400

    feat: adicionar getters e setters na classe Usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -21,6 +21,37 @@ public class Usuario {
         this.email = email;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     @Override
     public String toString() {
